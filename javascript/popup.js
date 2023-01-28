a0a016824e3ed3a4239fd190a54ea675b5bd0aa9
Mareta <maretahandanari@gmail.com>
2023-01-28 23:55:22 +0700

    add some features

---
+++ javascript/popup.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementById("close").onclick = function() {
+        document.getElementById("overlay").style.display = "none";
+    };
+};
+
+
+document.getElementById("add").onclick = function() {
+    document.getElementById("sidebar").style.display = "block";
+};
